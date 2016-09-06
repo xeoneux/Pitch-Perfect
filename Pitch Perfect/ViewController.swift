79d9909f6c68fadad3f1bbe3e2f829575d15f9c0
Aayush Kapoor <xeoneux@icloud.com>
2016-09-06 22:46:25 +0530

    feat: add logic for record buttons

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -19,17 +19,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        stopRecordingButton.enabled = false
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func recordAudio(sender: AnyObject) {
-        self.recordingLabel.text = "Recording in progress"
+        recordButton.enabled = false
+        stopRecordingButton.enabled = true
+        recordingLabel.text = "Recording in progress"
     }
 
     @IBAction func stopRecording(sender: AnyObject) {
-        print("Stop Recording button pressed")
+        recordButton.enabled = true
+        stopRecordingButton.enabled = false
+        recordingLabel.text = "Tap to Record"
     }
 
 }
