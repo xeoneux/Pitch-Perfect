b251c53615a77e73c316461f41d4fd7ab503734b
Aayush Kapoor <xeoneux@icloud.com>
2016-09-07 09:03:37 +0530

    feat: connect playback buttons

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
 
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var darthVaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+
     var recordedAudioURL: NSURL!
+
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("Playback!")
+    }
+
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("Stop!")
+    }
 
 }
