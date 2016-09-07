0096f3c9ce0dde45610a33598255d451c009001c
Aayush Kapoor <xeoneux@icloud.com>
2016-09-07 09:33:11 +0530

    feat: add audio playback

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -40,11 +40,27 @@ class PlaySoundsViewController: UIViewController {
     }
 
     @IBAction func playSoundForButton(sender: UIButton) {
-        print("Playback!")
+        switch ButtonType(rawValue: sender.tag)! {
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .DarthVader:
+            playSound(pitch: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+
+        configureUI(.Playing)
+
     }
 
     @IBAction func stopButtonPressed(sender: AnyObject) {
-        print("Stop!")
+        stopAudio()
     }
 
 }
